03c724c11d168edd7d14f40e1830d59eb737c967
Sam Security <silvaneimartins_rcc@hotmail.com>
2020-04-03 22:58:36 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+* Desenvolvedor: Silvanei Martins;
+* Email: silvaneimartins_rcc@hotmail.com;
+* WhatsApp: (69) 9.8405-2620;  
+* App Jogo Campo Minado;
+*/
+
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
